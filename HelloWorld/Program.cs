764fb23c815fff99cf14e90e8fa6e208f9c4514f
issamvandecoderclass <issam.buamran@coderclass.nl>
2019-04-09 15:24:45 +0200

    producten

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,15 +10,19 @@ namespace Boodschappen
             int aantal;
             float prijsExclBtwK = 3.95f;
             float prijsExlbtwT = 4.12f;
+            float prijsExlbtwS = 5.24f;
+            float prijsExlbtwK = 1.45f;
+            float prijsExlbtwD = 3.42f;
+            float prijsExlbtwW = 3.92f;
 
             float btwPercentage = 0.21f;
 
             Console.WriteLine("Een pakje koffie kost " + prijsExclBtwK + " ex btw.");
-            Console.WriteLine("Een pakje  kost " + prijsExclBtwT +" ex btw.");
-            Console.WriteLine("Een pakje  kost " + prijsExclBtwT + " ex btw.");
-            Console.WriteLine("Een pakje  kost " + prijsExclBtwT + " ex btw.");
-            Console.WriteLine("Een pakje  kost " + prijsExclBtwT + " ex btw.");
-            Console.WriteLine("Een pakje  kost " + prijsExclBtwT + " ex btw.");
+            Console.WriteLine("Een pakje thee kost " + prijsExclBtwT +" ex btw.");
+            Console.WriteLine("Een pakje sigaretten kost " + prijsExclBtwS + " ex btw.");
+            Console.WriteLine("Een pakje koekjes kost " + prijsExclBtwK + " ex btw.");
+            Console.WriteLine("Een pakje drinken kost " + prijsExclBtwD + " ex btw.");
+            Console.WriteLine("Een pakje wafels kost " + prijsExclBtwW + " ex btw.");
 
             aantal = Convert.ToInt32(Console.ReadLine());
             float totaalPrijs = aantal * prijsExclBtw + (aantal * prijsExclBtw) * btwPercentage;
