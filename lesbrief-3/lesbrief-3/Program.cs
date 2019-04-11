b27740e8b05364aa4b8ba3d44b8dafa73177dbd8
issamvandecoderclass <issam.buamran@coderclass.nl>
2019-04-11 09:17:34 +0200

    calculator werkt

---
+++ lesbrief-3/lesbrief-3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesbrief_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vul hier je eerste getal in: ");
+            int getal1 = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Wat is je tweede getal?: ");
+            int getal2 = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Kies hieruit: x,/,+,-");
+            char som = Convert.ToChar(Console.ReadLine());
+
+            if (som == 'x')
+            {
+                Console.WriteLine(getal1 * getal2);
+                Console.ReadLine();
+            }
+            else if (som == '/')
+            {
+                Console.WriteLine(getal1 / getal2);
+                Console.ReadLine();
+            }
+            else if (som == '+')
+            {
+                Console.WriteLine(getal1 + getal2);
+                Console.ReadLine();
+            }
+            else if (som == '-')
+            {
+                Console.WriteLine(getal1 - getal2);
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("Bro, wat heb je nou geschreven??!");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
