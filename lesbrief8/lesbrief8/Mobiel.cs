0bb75d203f47194591328b2620c937d1b007a9cb
issamvandecoderclass <issam.buamran@coderclass.nl>
2019-05-07 14:59:27 +0200

    lesbrief8 opdracht1 af

---
+++ lesbrief8/lesbrief8/Mobiel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesbrief8
+{
+    class Mobiel
+    {
+        public string Type;
+        public string Model;
+        public string OS;
+    }
+}
---
+++ lesbrief8/lesbrief8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lesbrief8
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Mobiel mobiel1 = new Mobiel();
+            mobiel1.Type = "Iphone";
+            mobiel1.Model = "Iphone X";
+            mobiel1.OS = "Apple IOS";
+
+            Console.WriteLine(mobiel1.Type);
+            Console.WriteLine(mobiel1.Model);
+            Console.WriteLine(mobiel1.OS);
+
+            Console.ReadLine();
+        }
+    }
+}
