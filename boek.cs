ce0d7a106aac58c5bccd043de00e8635a974005a
Issam <issam.buamran@coderclass.nl>
2019-06-27 15:04:29 +0200

    Lesbrief8-2

---
+++ boek.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Boek   
+{
+	public Boek()
+	{
+        public string titel;
+        public string auteur;
+        public string uitgever;
+        public int aantalPaginas;
+    }
+}
---
+++ lesbrief8-2/lesbrief8-2/mobiel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesbrief8_2
+{
+    class Mobiel
+    {
+        public string Type;
+        public string Model;
+        public string OS;
+    }
+}
