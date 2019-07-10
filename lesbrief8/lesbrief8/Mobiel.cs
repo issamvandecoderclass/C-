7edddf750a72add6a63a8deb841ce800eccb3b48
Issam van de CoderClass <issam.buamran@coderclass.nl>
2019-07-10 10:32:52 +0200

    Nu vraagt hij ook de aantal telefoons

--- lesbrief8/lesbrief8/Mobiel.cs
+++ lesbrief8/lesbrief8/Mobiel.cs
@@ -1,22 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace lesbrief8
+﻿namespace lesbrief8
 {
     class Mobiel
     {
-        public string Type;
-        public string Model;
-        public string OS;
+        const float btw = 0.21f;
+        public float prijs = 3.95f;
+        private string Type;
+        private string Model;
+        private string OS;
 
         public Mobiel(string nType, string nModel, string nOS)
         {
             type = nType;
             model = nModel;
             os = nOS;
+        }
+
+        public float Btw(int aantal)
+        {
+            return aantal * prijs + ((aantal * prijs) * btw);
+        }
+
+        public void SetPrijs(float nPrijs)
+        {
+            prijs = nPrijs;
         }
     }
 }
--- lesbrief8/lesbrief8/Program.cs
+++ lesbrief8/lesbrief8/Program.cs
@@ -9,9 +9,22 @@ namespace lesbrief8
         {
             Mobiel mobiel1 = new Mobiel("Iphone", "Iphone X", "Apple IOS");
             Mobiel mobiel2 = new Mobiel("Galaxy", "Samsung S9", "Android");
+            mobiel1.SetPrijs(1200f);
+            mobiel2.SetPrijs(850f);
 
-            Console.WriteLine(Mobiel1);
-            Console.WriteLine(mobiel2);
+            boek1.prijs = 4.95f;
+            boek2.prijs = 5.95f;
+
+            Console.WriteLine("Hoeveel Apple " + mobiel1.nModel + " wilt u kopen?");
+            int aantalIX = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hoeveel Samsung " + mobiel2.nModel + " wilt u kopen?");
+            int aantalSG = Convert.ToInt32(Console.ReadLine());
+
+            float totaalPrijsIX = mobiel1.Btw(aantalIX);
+            float totaalPrijsSG = mobiel2.Btw(aantalSG);
+
+            Console.WriteLine(aantalIX + " Apple telefoon(s) kosten in totaal " + totaalPrijsIX);
+            Console.WriteLine(aantalSG + " Samsung telefoon(s) kosten in totaal " + totaalPrijsSG);
 
             Console.ReadLine();
         }
